fa7fde2d8bafcb511bc3db17f35bb9220f6d225e
M. Zulqarnain <zulqarnain.mailbox@gmail.com>
2021-06-14 18:52:56 +0500

    testing xsscommitlint

--- scripts/xss-commit-linter.sh
+++ scripts/xss-commit-linter.sh
@@ -66,7 +66,7 @@ current_branch_hash=`git rev-parse HEAD`
 echo "past main commit"
 echo "${MAIN_COMMIT+x}"
 echo "past target branch"
-echo ""${TARGET_BRANCH+x}"
+echo "${TARGET_BRANCH+x}"
 echo $current_branch_hash
 
 if [ -z "${MAIN_COMMIT+x}" ]; then
